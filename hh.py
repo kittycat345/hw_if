f973c81328000ebc27816431027ce82d4dc81d53
kittycat345 <ilyapavl0v531@gmail.com>
2024-05-24 13:44:23 +0300

    Add files via upload

---
+++ hh.py
@@ -0,0 +1,31 @@
+print('1 блок')
+x=38
+print("дратути")
+if x<0:
+    print('Меньше нуля')
+print("датвидания")
+#dfdd
+print('2 блок')
+a,b= 10,5
+if a>b:
+    print("a>b")
+if a>b and a>0:
+    print("успех")
+if a>b and (a > 0 or b<1000):
+    print('успех')
+if 5<b and b<10:
+    print('успех')
+print('3 блок')
+if '34'>'123':
+    print('успех')
+if '123'>'12':
+    print('успех')
+if [1,2]>[1,1]:
+    print('успех')
+print('4 блок')
+# if "6">5:
+#    print('успех')
+#if [7,6]>6:
+#   print('успех')
+if '6'!=5:
+    print('успех')
